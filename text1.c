b1274cfc374005aecf4dda9e93aadb253c80b7d4
One <1427370978@qq.com>
2025-11-26 12:21:09 +0000

    update

---
+++ text1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+int p(char a[][1000],int n)
+{
+    char b[1000];
+	for(int i=0;i<n-1;i++)
+	{
+		int k=0;
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(strcmp(a[j],a[j+1])>0)
+			{
+				strcpy(b,a[j]);
+				strcpy(a[j],a[j+1]);
+				strcpy(a[j+1],b);
+				k=1;
+			}
+		}
+		if(k==0)
+			break;
+	}
+}	
+int main()
+{
+	int n;
+	printf("Please enter n strings");
+	char a[n][1000];
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s",a[i]);
+
+	}
+	printf("Before sorting.");
+	for(int i=0;i<n;i++)
+	{
+		printf("%s",a[i]);
+	}
+
+	printf("After sorting.");
+	p(a,n);
+	for(int i=0;i<n;i++)
+	{
+	  printf("%s",a[i]);
+	}
+	return 0;
+}
